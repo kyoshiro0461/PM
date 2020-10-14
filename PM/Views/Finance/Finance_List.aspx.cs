ea877a6c6797754dd98e5e839eba6d378ca37b6e
Administrator <Administrator@UFSERVER>
2020-10-14 17:38:08 +0800

    增加数据库备份

--- PM/Views/Finance/Finance_List.aspx.cs
+++ PM/Views/Finance/Finance_List.aspx.cs
@@ -7,7 +7,7 @@ using System.Web.UI.WebControls;
 
 namespace PM.Views.Finance
 {
-    public partial class Finance_List : System.Web.UI.Page
+    public partial class Finance : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
